ad18667f024c02cd5d38b93d9ae5efcca5fbb729
mehm8128 <mehm8128@gmail.com>
2023-08-09 22:21:25 +0900

    eslintのルール色々増やした

--- src/libs/mocks/main.ts
+++ src/libs/mocks/main.ts
@@ -9,6 +9,7 @@ export const initMock = () => {
 	if (process.env.NODE_ENV === 'development') {
 		if (typeof window !== 'undefined') {
 			const worker = setupWorker(...handlers(getApiOrigin()))
+			// eslint-disable-next-line @typescript-eslint/no-floating-promises
 			worker.start()
 		} else {
 			const server = setupServer(...handlers(getApiOrigin()))
