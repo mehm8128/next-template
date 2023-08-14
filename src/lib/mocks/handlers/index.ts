d3cabac4ce437f7729dc06410df6be6610a88ce4
mehm8128 <mehm8128@gmail.com>
2023-08-14 23:36:23 +0900

    fix getHandlersArray

--- src/lib/mocks/handlers/index.ts
+++ src/lib/mocks/handlers/index.ts
@@ -3,10 +3,9 @@ import { RestHandler } from 'msw'
 import { sampleHandlers } from './sample'
 
 export function getHandlersArray(
-	handlers: Record<
-		string,
-		(arg?: Partial<Record<string, unknown>>) => RestHandler
-	>,
+	// TODO: 配列とオブジェクトに制限したい
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	handlers: Record<string, (arg?: any) => RestHandler>,
 ): RestHandler[] {
 	return Object.values(handlers).map(handler => handler())
 }
