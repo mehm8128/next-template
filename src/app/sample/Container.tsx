dee765183a453a26e9033d3acc2257d1733fd553
mehm8128 <mehm8128@gmail.com>
2024-04-16 00:49:18 +0900

    SuspenseWithErrorBoundary

--- src/app/sample/Container.tsx
+++ src/app/sample/Container.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import { ErrorBoundary } from '@/components/ErrorBoundary'
+import SuspenseWithErrorBoundary from '@/components/SuspenseWithErrorBoundary'
 import Sample from '@/features/sample/components/Sample'
-import { Suspense } from 'react'
 
 export default function Container() {
 	return (
 		<div>
 			aaa
-			<ErrorBoundary fallback={<div>Something went wrong</div>}>
-				<Suspense fallback={<div>loading...</div>}>
-					<Sample />
-				</Suspense>
-			</ErrorBoundary>
+			<SuspenseWithErrorBoundary fallback={<div>loading...</div>}>
+				<Sample />
+			</SuspenseWithErrorBoundary>
 		</div>
 	)
 }
--- src/components/ErrorBoundary.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react'
-import { ErrorBoundary as BaseErrorBoundary } from 'react-error-boundary'
-
-export function ErrorBoundary({
-	fallback,
-	children
-}: { fallback: ReactElement; children: React.ReactNode }) {
-	return <BaseErrorBoundary fallback={fallback}>{children}</BaseErrorBoundary>
-}
