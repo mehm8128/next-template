35ca3b141f2ce08adde29b055bf2f40cd1f6102e
mehm8128 <mehm8128@gmail.com>
2024-04-16 00:50:16 +0900

    JotaiProvider

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -3,6 +3,7 @@
 import { KumaRegistry } from '@kuma-ui/next-plugin/registry'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
+import { Provider as JotaiProvider } from 'jotai'
 
 import { initMock } from '@/lib/mock'
 
@@ -13,9 +14,11 @@ const queryClient = new QueryClient()
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<KumaRegistry>
-			<QueryClientProvider client={queryClient}>
-				<ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-			</QueryClientProvider>
+			<JotaiProvider>
+				<QueryClientProvider client={queryClient}>
+					<ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+				</QueryClientProvider>
+			</JotaiProvider>
 		</KumaRegistry>
 	)
 }
